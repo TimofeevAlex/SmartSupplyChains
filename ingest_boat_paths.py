ffcbc1042e253878b83cca7f5de98e20e9729915
Oganes Manasian <36588698+oganesManasian@users.noreply.github.com>
2022-09-18 02:23:53 +0200

    Added risk analysis

--- ingest_boat_paths.py
+++ ingest_boat_paths.py
@@ -1,11 +1,14 @@
 from google.cloud import firestore
-from prepare_routes import prepare_routes
+from prepare_routes import get_routes_and_risks, get_current_routes_and_risks
 
 db = firestore.Client()
 db_paths = firestore.Client()
 
-routes, upsampled_routes = prepare_routes(upsample=True)
-  
+cur_datetime = datetime.datetime.now()
+df_bestellu, df_shiptrac, df_bestellu_plus_raw, all_risks = get_routes_and_risks()
+routes, upsampled_routes, risks = get_current_routes_and_risks(df_bestellu, df_shiptrac, df_bestellu_plus_raw,
+                                                               all_risks, cur_datetime, upsample=True)
+
 col_ref = db.collection('connected')
 col_ref_paths = db_paths.collection('paths')
 batch = db.batch()
@@ -21,13 +24,13 @@ try:
             path_x = [x for x, _ in list(route['path'])]
             path_y = [y for _, y in list(route['path'])]
             batch_paths.set(doc_ref_paths, {
-            'path_x': path_x,
-            'path_y': path_y,
-            'color': [256, 0, 128] if route['in_danger'] else [0, 256, 128],
-            'desc': route['desc'],
+                'path_x': path_x,
+                'path_y': path_y,
+                'color': [256, 0, 128] if route['in_danger'] else [0, 256, 128],
+                'desc': route['desc'],
             })
         batch_paths.commit()
-    while True:  
+    while True:
         ts += 1
         print(ts)
         for route_idx, route in enumerate(upsampled_routes if upsampled_routes else routes):
@@ -35,10 +38,10 @@ try:
             doc_ref = col_ref.document(str(int(route['name'])))
             doc_ref_paths = col_ref_paths.document(str(int(route['name'])))
             batch.set(doc_ref, {
-            'lonlat': list(route['path'][curr_ts]),
-            'timestamp': curr_ts
+                'lonlat': list(route['path'][curr_ts]),
+                'timestamp': curr_ts
             })
         batch.commit()
-        
+
 except KeyboardInterrupt:
     pass
